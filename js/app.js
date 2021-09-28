0956864a7946fbc19d70d265d1725155ec621bb4
Alejandro Butron <alesvoy@gmail.com>
2021-09-28 18:28:37 -0500

    Finished styling and added basic js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const App = (() => {
+  // Caching the DOM
+  const displayEl = document.querySelector("h1");
+  const addBtn = document.querySelector(".addition");
+  const subBtn = document.querySelector(".subtract");
+  const resetBtn = document.querySelector(".reset");
+
+  function init() {}
+})();
