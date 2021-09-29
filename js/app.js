bafaa179df2d8dab6eaa16edae3b2b2f09016f04
Alejandro Butron <alesvoy@gmail.com>
2021-09-28 20:12:25 -0500

    Finished project

--- js/app.js
+++ js/app.js
@@ -4,6 +4,41 @@ const App = (() => {
   const addBtn = document.querySelector(".addition");
   const subBtn = document.querySelector(".subtract");
   const resetBtn = document.querySelector(".reset");
+  let num = 0;
 
-  function init() {}
+  function reset() {
+    return 0;
+  }
+
+  function eventListeners() {
+    addBtn.addEventListener("click", function () {
+      num++;
+      render(num);
+    });
+
+    subBtn.addEventListener("click", function () {
+      num--;
+      render(num);
+    });
+
+    resetBtn.addEventListener("click", function () {
+      num = reset();
+      render(num);
+    });
+  }
+
+  function render(num = 0) {
+    displayEl.innerHTML = `${num}`;
+  }
+
+  function init() {
+    eventListeners();
+    render(num);
+  }
+
+  return {
+    init: init,
+  };
 })();
+
+App.init();
